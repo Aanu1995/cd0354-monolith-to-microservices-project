0cfe0f335883d3b3f97be1a2e2a47eecfbfcb5c3
Aanu Olakunle <olakunleaanu@gmail.com>
2022-06-22 09:41:07 +0100

    update frontend

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a8f604b7731cc4cd5a90b97f99626216-2121674157.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
