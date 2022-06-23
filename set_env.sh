1266625d069905647e84f4f9d358dbd532aeb904
Aanu Olakunle <olakunleaanu@gmail.com>
2022-06-23 08:37:26 +0100

    bugfix

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=annulusudagram
-export POSTGRES_PASSWORD=OAPpelumi1
-export POSTGRES_DATABASE=annulusudagram
-export POSTGRES_HOST=annulusudagram.cb0xrsr88syy.us-east-1.rds.amazonaws.com
-export AWS_S3_BUCKET=annulus-udagram
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=annulusudagram
+export POSTGRES_USERNAME=""
+export POSTGRES_PASSWORD=""
+export POSTGRES_DATABASE=""
+export POSTGRES_HOST=""
+export AWS_S3_BUCKET=""
+export AWS_REGION=""
+export AWS_PROFILE=""
+export JWT_SECRET=""
 export URL=http://localhost:8100
